461edd2c335eb38b3e91d118b898610ddf3616bf
Andreas Schürmann <andreas@adhook.io>
2022-02-12 11:53:42 +0100

    Initial Commit

--- android/src/main/java/com/metricool/plugins/instagram/InstagramPlugin.java
+++ android/src/main/java/com/metricool/plugins/instagram/InstagramPlugin.java
@@ -1,9 +1,11 @@
 package com.metricool.plugins.instagram;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.util.Log;
 
+import androidx.activity.result.ActivityResult;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
@@ -12,22 +14,17 @@ import com.getcapacitor.NativePlugin;
 import com.getcapacitor.Plugin;
 import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.ActivityCallback;
+import com.getcapacitor.annotation.CapacitorPlugin;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Locale;
 
-@NativePlugin(
-    requestCodes = {
-        InstagramPlugin.REQUEST_SHARE
-    }
-)
+@CapacitorPlugin(name = "Instagram")
 public class InstagramPlugin extends Plugin {
 
     private static final String LOGTAG = "InstagramPlugin";
-    protected static final int REQUEST_SHARE = 2020;
-
-    private static final String MEDIA_IMAGE = "image";
-    private static final String MEDIA_VIDEO = "video";
 
     private static final String TARGET_FEED = "feed";
     private static final String TARGET_STORY = "story";
@@ -35,100 +32,81 @@ public class InstagramPlugin extends Plugin {
     @PluginMethod
     public void shareLocalMedia(PluginCall call) {
         JSArray medias = call.getArray("medias");
-        if (medias == null || medias.length()==0) {
-            call.error("Missing 'medias' argument");
+        if (medias == null || medias.length() == 0) {
+            call.reject("Missing 'medias' argument");
             return;
-        };
+        }
 
         String mediaType = call.getString("mediaType");
-        if (mediaType == null || mediaType.length()==0) {
-            call.error("Missing 'mediaType' argument");
+        if (mediaType == null || mediaType.length() == 0) {
+            call.reject("Missing 'mediaType' argument");
             return;
-        };
+        }
+
 
         String target = call.getString("target");
-        if (target == null || target.length()==0) {
-            call.error("Missing 'target' argument");
+        if (target == null || target.length() == 0) {
+            call.reject("Missing 'target' argument");
             return;
-        };
+        }
 
         Log.d(LOGTAG, "shareLocalMedia: '" + medias + "', '" + mediaType + "', '" + target + "'");
         shareLocalMedia(call, medias, mediaType, target);
     }
 
-
-    @Override
-    protected void handleOnActivityResult(int requestCode, int resultCode, Intent data) {
-        super.handleOnActivityResult(requestCode, resultCode, data);
-
-        // Get the previously saved call
-        PluginCall call = getSavedCall();
-        if (call == null) {
-            return;
-        }
-
-        if (requestCode == REQUEST_SHARE) {
-            call.success();
-        } else {
-            call.error("Internal error. Unknown request code: " + requestCode);
-        }
-    }
-
-
     private void shareLocalMedia(PluginCall call, JSArray medias, String mediaType, String target) {
-        saveCall(call);
+        getBridge().saveCall(call);
 
         try {
             boolean multipleMedia = medias.length() > 1;
 
             // choose the right action
-            String action = Intent.ACTION_SEND;
+            String action = "com.instagram.share.ADD_TO_" + TARGET_FEED.toUpperCase();
             if (multipleMedia || TARGET_STORY.equalsIgnoreCase(target)) {
                 // multiple medias are only enable to share in 'stories', so 'target' parameter is discarded.
                 action = Intent.ACTION_SEND_MULTIPLE;
                 multipleMedia = true;
-            } else {
-                action = "com.instagram.share.ADD_TO_" + TARGET_FEED.toUpperCase();
             }
 
-            Intent  shareIntent = new Intent(action);
+            Intent shareIntent = new Intent(action);
+            if (mediaType.toLowerCase().contains("mp4") || mediaType.toLowerCase().contains("mov")) {
+                shareIntent.setType("video/" + mediaType);
+            } else {
+                shareIntent.setType("image/" + mediaType);
+            }
 
             if (multipleMedia) {
                 ArrayList<Uri> uris = new ArrayList<Uri>();
 
-                for (int i=0; i<medias.length(); i++) {
+                for (int i = 0; i < medias.length(); i++) {
                     File file = new File(medias.getString(i));
-                    Uri uri = getUriForFile(file);
+                    Uri uri = Uri.fromFile(file);
                     uris.add(uri);
                 }
                 shareIntent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, uris);
-                shareIntent.setType(mediaType + "/*");
             } else {
                 File file = new File(medias.getString(0));
-                Uri uri = getUriForFile(file);
-
-                shareIntent.setType(mediaType + "/*");
+                Uri uri = Uri.fromFile(file);
                 shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
             }
 
             shareIntent.setPackage("com.instagram.android");
             shareIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
 
-            Log.v(LOGTAG, "Sharing '" + action + "', '"+ mediaType + "', '" + medias + "'");
-            startActivityForResult(call, shareIntent, REQUEST_SHARE);
+            Log.v(LOGTAG, "Sharing '" + action + "', '" + mediaType + "', '" + medias + "'");
+            startActivityForResult(call, shareIntent, "shareResultCallback");
+
         } catch (Exception e) {
-            call.error("Internal error: " + e.getMessage());
+            call.reject("Internal error: " + e.getMessage());
         }
     }
 
-    private Uri getUriForFile(File file) {
-        //Uri uri = Uri.fromFile(file);
-        AppCompatActivity activity = this.getActivity();
-
-        // Really not clean, as it depends on the package in our MainActivity program.
-        String authority = "com.ionicframework.metricool185346.fileprovider";
-
-        Uri uri = FileProvider.getUriForFile(activity, authority, file);
-        return uri;
+    @ActivityCallback
+    private void shareResultCallback(PluginCall call, ActivityResult result) {
+        if (result.getResultCode() == Activity.RESULT_CANCELED) {
+            call.reject("Activity canceled");
+        } else {
+            call.resolve();
+        }
     }
 }
